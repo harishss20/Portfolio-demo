b812650749735b7131965598544aa91fdbcb2793
HARISH SS <116174706+harishss20@users.noreply.github.com>
2024-10-29 13:28:12 +0530

    deployment process

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import path from "path"; // Import the path module
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
